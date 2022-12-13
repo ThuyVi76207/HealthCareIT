a75e85385329a00fda26059732c8ba4efb004843
thuyvy76207 <1951052237vi@ou.edu.vn>
2022-12-13 08:44:05 +0700

    Update code

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,103 @@
+import './NavbarStyle.scss';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+// import { FormattedMessage } from 'react-intl';
+// import LogoImg from '../../assets/Hcare.svg';
+
+import { useState } from 'react';
+import i18n from '../../function/i18n/i18n';
+import { withNamespaces } from 'react-i18next';
+
+
+
+const Navbar = ({ t }) => {
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    }
+
+    const [start, setStart] = useState(false);
+    // const secRef = useRef();
+    useEffect(() => {
+        // Define the on-scroll callback
+        const callback = function () {
+            // const secTop = secRef.current.offsetTop;
+            if (window.scrollY >= 100) {
+                setStart(true);
+            }
+            else {
+                setStart(false);
+            }
+        };
+
+        // Attach the callback after the component mounts
+        window.addEventListener("scroll", callback);
+
+        // Detach the callback before the component unmounts
+        return () => window.removeEventListener("scroll", callback);
+    }, []);
+    console.log(start)
+
+    return (
+        <div className='navbar-common'>
+            <div className='navbar-up'>
+                <div className='header-up'>
+                    <img className='h-[3.125rem]' src='/logo/Hcare.svg' alt='' />
+                    <a href="mailto:healthcare@gmail.com" className='content-item'>
+                        <i className='fa fa-envelope'></i>
+                        <div className='text-item'>healthcare@gmail.com</div>
+                    </a>
+
+                    <a href="tel:0123456894" className='content-item'>
+                        <i className='fa fa-phone'></i>
+                        <div className='text-item'>0123456894</div>
+                    </a>
+                    <div className='content-item'>
+                        <i className='fa fa-registered'></i>
+                        <div className='text-item'>{t('navbar.register')}</div>
+                    </div>
+                    <div className='content-item'>
+                        <i className='fa fa-user'></i>
+                        <div className='text-item'>{t('navbar.login')}</div>
+                    </div>
+                </div>
+            </div>
+            <div className={start ? 'navbar-down sticky' : 'navbar-down'} id='menuHeader'>
+                <div className='header-down'>
+                    <div className='left-content'>
+                        <div className='child-content'>
+                            <Link to={"/home"}><b>{t('navbar.homepage')}</b></Link>
+                        </div>
+                        <div className='child-content'>
+                            <Link to={"/room"}><b>{t('navbar.onlinemedica')}</b></Link>
+                        </div>
+                        <div className='child-content'>
+                            <Link to="#"><b>{t('navbar.forum')}</b></Link>
+                        </div>
+                        <div className='child-content'>
+                            <Link to="#"><b>{t('navbar.healthnews')}</b></Link>
+                        </div>
+                        <div className='child-content'>
+                            <Link to={"/contact"}><b>{t('navbar.contact')}</b></Link>
+                        </div>
+
+                    </div>
+                    <div className='change-language'>
+                        <i className='text-[20px]'>
+                            <ion-icon name="earth-outline"></ion-icon>
+                        </i>
+
+                        <div><span onClick={() => changeLanguage('vi')}>VN /</span></div>
+                        <div><span onClick={() => changeLanguage('en')}>EN</span></div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default withNamespaces()(Navbar)
+
+
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-const Home = () => {
+// import i18n from '../function/i18n/i18n';
+// import { withNamespaces } from 'react-i18next';
+import Navbar from "../components/Navbar/Navbar";
+import { withNamespaces } from "react-i18next";
+
+const Home = ({ t }) => {
+    // const changeLanguage = (lng) => {
+    //     i18n.changeLanguage(lng);
+    // }
     return (
-        <div className="text-2xl">Homewvdghsdvbsd sdbfvgdsvfhvsdf bs fvdfhRea</div>
+        <>
+            <Navbar />
+
+        </>
+
+
+        // <div>
+        //     <button onClick={() => changeLanguage('vi')}>vi</button>
+        //     <button onClick={() => changeLanguage('en')}>en</button>
+        //     <h1>{t('Welcome to React')}</h1>
+        // </div>
     )
 }
 
-export default Home
+export default withNamespaces()(Home)
