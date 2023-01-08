0cc9ad1805679d8190ba15be25f7cf3e380b3d1a
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-01-08 08:59:26 +0700

    Fix position navbar

--- src/features/User/features/Home/SectionData/Section3.jsx
+++ src/features/User/features/Home/SectionData/Section3.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { withNamespaces } from "react-i18next";
 import Specialty from "./Specialty/Specialty";
 
-function Section3() {
+function Section3({ t }) {
     return (
         <div className="section3">
+            <div>
+                <h2>{t('section3.title')}</h2>
+            </div>
             <Specialty />
         </div>
     )
 }
 
-export default Section3;
+export default withNamespaces()(Section3);
