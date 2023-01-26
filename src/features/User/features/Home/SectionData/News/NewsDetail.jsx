e8750a3a40cfb44563da18037395b7c168f3bfe4
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-01-26 11:54:33 +0700

    Page Detail News

--- src/features/User/features/Home/SectionData/News/NewsDetail.jsx
+++ src/features/User/features/Home/SectionData/News/NewsDetail.jsx
@@ -4,13 +4,14 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { withNamespaces } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { getAllNewsById } from 'services/userService';
+import { getAllNews, getAllNewsById } from 'services/userService';
 import './NewsDetailStyles.scss';
 import { Buffer } from 'buffer';
 
 const NewsDetail = ({ t }) => {
     const { id } = useParams();
     const [dataDetailNews, setDataDetailNews] = useState({});
+    const [dataListNews, setDataListNews] = useState([]);
     // console.log('Check id param', id);
 
     useEffect(() => {
@@ -33,6 +34,19 @@ const NewsDetail = ({ t }) => {
         printNewsDetail();
     }, [id]);
 
+    useEffect(() => {
+        const printListNews = async () => {
+            try {
+                const resListNews = await getAllNews();
+                console.log('Check list', resListNews)
+            } catch (error) {
+                console.log("Failed to get API list News", error);
+            }
+        }
+
+        printListNews();
+    }, [])
+
     let imageBase64 = '';
     if (dataDetailNews.image) {
         imageBase64 = Buffer.from(dataDetailNews.image, 'base64').toString('binary');
@@ -43,31 +57,39 @@ const NewsDetail = ({ t }) => {
 
     return (
         <MainLayout>
-            <div className='news-detail'>
-                <div className='flex gap-[5%]'>
-                    <div className='news-detail__left'>
-                        {
-                            dataDetailNews &&
-                            <div>
-                                <h2 className='title-new'>{dataDetailNews.name}</h2>
-                                <div className='flex mt-2 text-gray-400'>{t('detailnews.createdat')}
-                                    <p className='px-2'>{createDate}</p>{t('detailnews.updatedat')}<p className='px-2'>{updateDate}</p>
-                                </div>
+            <div className='bg-[#f7f7f7] pt-14'>
+                <div className=''>
 
-                                <div
-                                    className='content'
-                                    style={{ backgroundImage: `url(${imageBase64})` }}>
-
-                                </div>
-
-                                <div dangerouslySetInnerHTML={{ __html: dataDetailNews.descriptionHTML }}></div>
-                            </div>
-                        }
-                    </div>
-                    <div className='news-detail__right'>Tin tuc moi</div>
                 </div>
+                <div className='news-detail'>
+                    <div className='flex gap-[3%] p-6'>
+                        <div className='news-detail__left'>
+                            {
+                                dataDetailNews &&
+                                <div>
+                                    <h2 className='title-new'>{dataDetailNews.name}</h2>
+                                    <div className='flex mt-2 text-gray-400'>{t('detailnews.createdat')}
+                                        <p className='px-2'>{createDate}</p>{t('detailnews.updatedat')}<p className='px-2'>{updateDate}</p>
+                                    </div>
 
+                                    <div
+                                        className='content'
+                                        style={{ backgroundImage: `url(${imageBase64})` }}>
+
+                                    </div>
+
+                                    <div className='description-news' dangerouslySetInnerHTML={{ __html: dataDetailNews.descriptionHTML }}></div>
+                                </div>
+                            }
+                        </div>
+                        <div className='news-detail__right'>
+                            <h2 className='text-[20px] uppercase font-bold text-[#333] mt-[40px]'>{t('detailnews.more')}</h2>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
         </MainLayout>
     )
 }
