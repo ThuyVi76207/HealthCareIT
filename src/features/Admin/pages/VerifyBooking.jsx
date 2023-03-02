cfd42f2a8344e8ad2f70a8685c58518c342631af
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-03-02 09:47:49 +0700

    Update ui page verify booking

--- src/features/Admin/pages/VerifyBooking.jsx
+++ src/features/Admin/pages/VerifyBooking.jsx
@@ -36,30 +36,30 @@ const VerifyBooking = ({ t }) => {
 
     return (
         <>
-            {/* <Loading loading={loading} /> */}
-            <div className="bg-[#d8f2f1] relative w-full h-[100vh]">
-                <div className="bg-white absolute top-[20%] left-[35%] w-[500px] h-[300px]">
-                    <div className="bg-[#16917c] h-[50%] flex items-center">
-                        <div className="bg-[#16917c] border-[3px] border-white mx-auto h-[80px] w-[80px] rounded-[50%] flex justify-center items-center">
-                            <i className="font-extrabold pt-1 pl-1 text-[45px] text-white"><ion-icon name="checkmark-outline"></ion-icon></i>
+            <Loading loading={loading} />
+
+            {
+                !loading
+                    ?
+                    <div className="bg-[#d8f2f1] relative w-full h-[100vh]">
+                        <div className="bg-white absolute top-[20%] left-[35%] w-[500px] h-[300px]">
+                            <div className="bg-[#16917c] h-[50%] flex items-center">
+                                <div className="bg-[#16917c] border-[3px] border-white mx-auto h-[80px] w-[80px] rounded-[50%] flex justify-center items-center">
+                                    <i className="font-extrabold pt-1 pl-1 text-[45px] text-white"><ion-icon name="checkmark-outline"></ion-icon></i>
+                                </div>
+                            </div>
+
+                            <div className="h-[50%] text-center">
+                                <h2 className="text-[25px] font-bold mt-3">{t('verifyBooking.title')}!</h2>
+                                <p className="font-medium mt-1">{t('verifyBooking.description')}</p>
+                                <p className="font-medium mt-1">{t('verifyBooking.note')}</p>
+                            </div>
+
                         </div>
                     </div>
-
-                    <div className="h-[50%] text-center">
-                        <h2 className="text-[25px] font-bold mt-3">{t('verifyBooking.title')}!</h2>
-                        <p className="font-medium mt-1">{t('verifyBooking.description')}</p>
-                        <p className="font-medium mt-1">{t('verifyBooking.note')}</p>
-                    </div>
-
-                </div>
-            </div>
-            {/* {
-                loading
-                    ?
-                    null
                     :
-                    <div className="bg-[#d8f2f1]">Xác nhận thành công</div>
-            } */}
+                    null
+            }
         </>
 
     )
