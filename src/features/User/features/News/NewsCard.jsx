eda192f81f87628d83499d58cb89c2ab40b0c88c
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-04-19 12:39:13 +0700

    fix

--- src/features/User/features/News/NewsCard.jsx
+++ src/features/User/features/News/NewsCard.jsx
@@ -1,30 +1,44 @@
-import { useNavigate } from "react-router-dom"
-import "./NewsCardStyles.scss"
+import { useNavigate } from "react-router-dom";
+import "./NewsCardStyles.scss";
 
 const NewsCard = ({ news }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleDetailNewsOnclick = () => {
-        navigate(`/healthcare/detail-news/${news.id}`)
-    }
-    return (
-        <div className="news">
-            <img onClick={() => handleDetailNewsOnclick()} style={{ width: "140px", height: "140px", cursor: "pointer" }} src={news.image} alt={news.name} />
-            <div>
-                <div className="news__overfow" >
-                    <h3 className="news__title hover:text-[#16917c] cursor-pointer" onClick={() => handleDetailNewsOnclick()}>{news.name}</h3>
-                    <div dangerouslySetInnerHTML={{ __html: news.descriptionHTML }} className='text-[15px]'></div>
-                </div>
-                .........
-                <div className="news__actions">
-
-                    <button onClick={() => handleDetailNewsOnclick()} className="font-semibold hover:text-red-500">Chi tiết</button>
-
-                </div>
-            </div>
-
+  const handleDetailNewsOnclick = () => {
+    navigate(`/healthcare/detail-news/${news.id}`);
+  };
+  return (
+    <div className="news">
+      <img
+        onClick={() => handleDetailNewsOnclick()}
+        src={news.image}
+        alt={news.name}
+      />
+      <div>
+        <div className="news__overfow">
+          <h3
+            className="news__title hover:text-[#16917c] cursor-pointer"
+            onClick={() => handleDetailNewsOnclick()}
+          >
+            {news.name}
+          </h3>
+          <div
+            dangerouslySetInnerHTML={{ __html: news.descriptionHTML }}
+            className="text-[15px]"
+          ></div>
         </div>
-    )
-}
+        .........
+        <div className="news__actions">
+          <button
+            onClick={() => handleDetailNewsOnclick()}
+            className="font-semibold hover:text-red-500"
+          >
+            Chi tiết
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NewsCard
+export default NewsCard;
