974dd5939df3050fc0011b7c216b8cbd06c51593
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-04-25 09:50:36 +0700

    fix chat

--- src/features/User/components/Chatbot/Chatbot.jsx
+++ src/features/User/components/Chatbot/Chatbot.jsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+
 const Chatbot = () => {
+  useEffect(() => {
+    var chatbox = document.getElementById("fb-customer-chat");
+    chatbox.setAttribute("page_id", "102440526142629");
+    chatbox.setAttribute("attribution", "biz_inbox");
+
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v16.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
   return (
     <>
       <div id="fb-root"></div>
