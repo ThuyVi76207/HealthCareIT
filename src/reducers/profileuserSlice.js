f2720174222b40b8df4a602d75425d51384d2158
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-05-31 21:04:07 +0700

    add permission super admin

--- src/reducers/profileuserSlice.js
+++ src/reducers/profileuserSlice.js
@@ -3,16 +3,16 @@ const { createSlice } = require("@reduxjs/toolkit");
 const initialState = {};
 
 const profileuserSlice = createSlice({
-    name: "profileuser",
-    initialState,
-    reducers: {
-        addProfileUser: (state, action) => {
-            state = action.payload;
-            state = { ...state, isLogin: true }
-            return state;
-        }
-    }
-})
+  name: "profileuser",
+  initialState,
+  reducers: {
+    addProfileUser: (state, action) => {
+      state = action.payload;
+      state = { ...state, isLogin: true };
+      return state;
+    },
+  },
+});
 
 export const { addProfileUser } = profileuserSlice.actions;
 export default profileuserSlice.reducer;
